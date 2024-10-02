1e341ec59832d52296fbdf63a09d8b15f8b97a92
Luiz Henrique Pasin Laikovski <luizlaikovski@gmail.com>
2024-10-02 15:00:10 -0300

    Criação dos atributos e métodos construct, getters e setters

--- AulasLucasSousa/src/Encapsulamento/ControleRemoto.java
+++ AulasLucasSousa/src/Encapsulamento/ControleRemoto.java
@@ -1,4 +1,37 @@
 package Encapsulamento;
 public class ControleRemoto {
+	// ATRIBUTOS
+	private int voleume;
+	private boolean ligado;
+	private boolean tocando;
+	
+	//MÉTODOS ESPECIAIS
+	// MÉTODO CONSTRUTOR
+	public ControleRemoto(int voleume, boolean ligado, boolean tocando) {
+		this.voleume = 50;
+		this.ligado = false;
+		this.tocando = false;
+	}
+	// MÉTODO GET E SET
 
+	public int getVoleume() {
+		return voleume;
+	}
+	public void setVoleume(int voleume) {
+		this.voleume = voleume;
+	}
+	public boolean isLigado() {
+		return ligado;
+	}
+	public void setLigado(boolean ligado) {
+		this.ligado = ligado;
+	}
+	public boolean isTocando() {
+		return tocando;
+	}
+	public void setTocando(boolean tocando) {
+		this.tocando = tocando;
+	}
+
+	
 }
